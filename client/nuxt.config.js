d493428c2d13d527bd1f6b9dc1e61f30bfcaf971
mizuki_r <ry.mizuki@gmail.com>
2018-07-16 18:31:00 +0900

    feat(client): faviconとdescriptionをよしなに

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -7,10 +7,11 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Kindleの新刊を探しやすく' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      // FIXME
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
